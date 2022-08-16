95a147905e4589caf4c1f11b08ca33d216207b51
Amanda Estrela <efernandes.amanda@gmail.com>
2022-08-15 23:45:03 -0300

    Next.JS

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -13,6 +13,7 @@ export default function Inicio() {
             <Navegador texto="Exemplo" destino='/exemplo' cor="#9400d3"/>
             <Navegador texto="JSX" destino='/jsx'  cor="coral" />
             <Navegador texto="Navegação #01" destino='/navegacao'  cor="crimson" />
+            <Navegador texto="Navegação #02" destino='/cliente/mg/256'  cor="green" />
         </div>
         )
 }
